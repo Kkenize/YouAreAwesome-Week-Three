5a5c359d5f2be073ececf62b981adfe04164d9e9
Kkenize <Kkenizez@gmail.com>
2025-02-01 16:21:18 -0500

    In this update, I added a toggler to allow control over whether a sound shoud be played in addition to simplifying the code by adding functions. More updates to come.

--- YouAreAwesome Week Three/ContentView.swift
+++ YouAreAwesome Week Three/ContentView.swift
@@ -1,23 +1,119 @@
 //
 //  ContentView.swift
-//  YouAreAwesome Week Three
+//  YouAreAwesome
 //
 //  Created by Zhejun Zhang on 2/1/25.
 //
 
 import SwiftUI
+import AVFAudio
 
 struct ContentView: View {
+    @State private var imageName = ""
+    @State private var imageRandomer = 0
+    @State private var lastImageRandomer = -1
+    @State private var messageString = ""
+    @State private var messageRandomer = 0
+    @State private var lastMessageRandomer = -1
+    @State private var imageCount = 0
+    @State private var messageCount = 0
+    @State private var audioPlayer: AVAudioPlayer!
+    @State private var soundName = ""
+    @State private var soundRandomer = 0
+    @State private var lastSoundRandomer = -1
+    @State private var soundOn = true
+    
+    func playSound(soundName: String) {
+        if audioPlayer != nil && audioPlayer.isPlaying {
+            audioPlayer.stop()
+        }
+        guard let soundFile = NSDataAsset(name: soundName) else {
+            print("Can't find \(soundName)")
+            return
+        }
+        do {
+            audioPlayer = try AVAudioPlayer(data: soundFile.data)
+            audioPlayer.play()
+        } catch {
+            print("\(error.localizedDescription)")
+            return
+        }
+    }
+    
+    func generateRandomNumber(max: Int, lastValue: Int) -> Int {
+        var returnValue = -1
+        repeat {
+            returnValue = Int.random(in: 0...max-1)
+        } while lastValue == returnValue
+        return returnValue
+    }
+    
     var body: some View {
+        
+        let messages = ["You are awesome!", "You are great!", "You are amazing!", "You are incredible!", "You are fantastic!", "You are perfect!", "You are the best!", "You are the GOAT!", "You are dazzling!"]
+        let numberOfImages = 9
+        
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            
+            Text(messageString)
+                .bold()
+                .font(.largeTitle)
+                .foregroundStyle(.yellow)
+                .minimumScaleFactor(0.5)
+                .multilineTextAlignment(.center)
+                .animation(.easeInOut(duration: 0.15), value: messageString)
+                .frame(height: 100)
+            
+            Spacer()
+            
+            Image(imageName)
+                .resizable()
+                .scaledToFit()
+                .clipShape(RoundedRectangle(cornerRadius: 50))
+                .animation(.default, value: imageName)
+                .shadow(radius: 50)
+            
+            Spacer()
+            
+            HStack {
+                Text("Sound On:")
+                Toggle("", isOn: $soundOn)
+                    .labelsHidden()
+                    .onChange(of: soundOn){
+                        if audioPlayer != nil && audioPlayer.isPlaying {
+                            audioPlayer.stop()
+                        }
+                    }
+                
+                Spacer()
+                
+                Button("Show Message") {
+                    messageRandomer = generateRandomNumber(max: messages.count, lastValue: lastMessageRandomer)
+                    messageString = messages[messageRandomer]
+                    lastMessageRandomer = messageRandomer
+                    
+                    imageRandomer = generateRandomNumber(max: numberOfImages, lastValue: lastImageRandomer)
+                    imageName = "image\(imageRandomer)"
+                    lastImageRandomer = imageRandomer
+                    
+                    soundRandomer = generateRandomNumber(max: 5, lastValue: lastSoundRandomer)
+                    soundName = "sound\(soundRandomer)"
+                    lastSoundRandomer = soundRandomer
+                    if soundOn {
+                        playSound(soundName: soundName)
+                    }
+                    
+                }
+                .buttonStyle(.borderedProminent)
+                .bold()
+                .font(.title2)
+                .tint(.blue)
+            }
         }
         .padding()
     }
 }
+
 
 #Preview {
     ContentView()
